9687f464056b6eb8c5597ed93b0606c17e2c51b6
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-05-11 17:54:07 +0600

    NewFeature

--- index.js
+++ index.js
@@ -2,10 +2,45 @@ const express = require('express')
 const app = express()
 const Posts = require('./api/model/posts')
 const postData = new Posts()
+const multer =require('multer')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
+    }
+})
+
+const getExt = (mimeType) => {
+    switch(mimeType) {
+        case "image/png":
+            return '.png';
+
+        case "image/jpg":
+            return ".jpg";
+
+        case "image/jpeg":
+            return '.jpeg'
+    }
+}
+
+var upload = multer({ storage: storage })
+
+
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
+})
+
+app.use('/uploads', express.static('uploads'))
 
 
 app.get('/', (req, res) => {
-    
     res.status(200).send(postData.get())
 })
 
@@ -23,4 +58,21 @@ app.get('/api/posts/:post_id', (req, res) => {
     }
 })
 
-app.listen(5050, () => console.log('listenning from port 3000...'))
+app.post('/api/posts', upload.single('post-image'), (req, res) => {
+    console.log(req.body)
+    console.log(req.file.path)
+    const filePath = req.file.path
+    const replaceFile = filePath.replace('\\', '/')
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title" : req.body.title,
+        "content" : req.body.content,
+        "added-date": `${Date.now()}`,
+        "post_image" : replaceFile
+    }
+    postData.add(newPost)
+
+    res.status(201).send('ok')
+})
+
+app.listen(5050, () => console.log('listenning from port 5050...'))
